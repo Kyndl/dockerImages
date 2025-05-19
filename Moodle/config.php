5134483671ec4ade0a20bb962fc7861314c6a598
Kyndl <rudolfkyndl@sosasou.cz>
2025-05-19 21:42:20 +0200

    maybe d

---
+++ Moodle/config.php
@@ -0,0 +1,26 @@
+<?php
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'db';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodle';
+$CFG->dbpass    = 'moodlepass';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://10.0.2.15:8080';
+$CFG->dataroot  = '/var/www/html/moodledata';
+$CFG->admin    = 'admin';
+
+$CFG->directorypermissions = 02777;
+
+require_once(__DIR__ . '/lib/setup.php');
